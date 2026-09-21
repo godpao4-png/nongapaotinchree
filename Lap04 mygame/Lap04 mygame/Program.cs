3fce74664c9d04742334ccf7d31fed6dc71c5ff1
godpao4-png <godpao4@gmail.com>
2026-09-21 10:34:21 +0700

    a

--- Lap04 mygame/Lap04 mygame/Program.cs
+++ Lap04 mygame/Lap04 mygame/Program.cs
@@ -1,57 +1,172 @@
-﻿namespace Lap04_mygame
+﻿
+//Student ID : 1690700081
+//Name       : kiart losatcha
+//Section    : 129A
+//No.        :
+//Course     : GI113 Computer Programming (GI113)
+
+using System;
+
+namespace Assignment1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            const string GameTitle = "marvel spider man miles morales"; 
-
-            var heroName = "miles morales";         
-            int heroLevel = 39;
-            int maxHp = 100;
-            int currentHp = 275;
-            float critChance = 2.15f;
-            float attackPower = 12.30f;
-            double staminaElectric = 50.5;  
-            bool isPlayable = true;
-
-            Console.WriteLine("<< \"=======================================================================\\n");
-            Console.WriteLine("<<  \"  ____ ____ ___ ____  _____ ____    ____  ___   _   _   _   _ \\n");
-            Console.WriteLine("<<  \" / ___|  _ \\_ _|  _ \\| ____|  _ \\\\  |  _ \\\\/ _ \\\\ | \\\\ | | / \\\\ | |\\");
-            Console.WriteLine("<<  \" \\___ \\ |_) | || | | |  _| | |_) | | |_) | | | ||  \\\\| |/ _ \\\\| |\\n");
-            Console.WriteLine(" << \"  ___) |  __/| || |_| | |___|  _ <  |  __/| |_| || |\\\\  / ___ \\\\_|\\n\"");
-            Console.WriteLine(" << \" |____/|_|  |___|____/|_____|_| \\_\\ |_|    \\\\___/ |_| \\\\_/_/   \\\\_|\\n");
-            Console.WriteLine(" << \" [ FRIENDLY NEIGHBORHOOD SPIDER-MAN HUD - MILES MORALES SYSTEM ]\\n");
-            Console.WriteLine("<< \"=======================================================================\\n\\n\\");
            
-           Console.WriteLine(" << \" | SPIDER-SUIT PROFILE                                                 |\n");  
-            Console.WriteLine($" | >> HERO ALIAS  : Miles Morales (Spider-Man)                        \n");
-            Console.WriteLine($" | >>Level       : {heroLevel}                                        \n        ");
-            Console.WriteLine($" | >>Hp: {currentHp} / {maxHp}                                       \n        ");
-            Console.WriteLine($" | >>Max HP      : {maxHp}                                            \n        ");
-            Console.WriteLine($" | >>Crit Chance : {critChance}                                       \n        ");
-            Console.WriteLine($" | >>Attack Power: {attackPower}                                      \n        ");
-            Console.WriteLine($" | >>Stamina     : {staminaElectric}                                  \n        ");
-            Console.WriteLine($" | >>Playable    : {isPlayable}                                                                  ");
-            Console.WriteLine(" << \"---------------------------------------------------------------------------------------");
+            const string GameTitle = "HELLDIVERS 2";
+
+            
+            var helldiverName = "Super Citizen";     
+            var soldierClass = 'H';                  
+
+            
+            string activeMission = "LIBERATE SUPER EARTH"; 
+            char armorGrade = 'A';                         
+            int requisitionSlips = 12500000;               
+            float combatSpeed = 7.85f;                     
+            double stratagemRange = 125.75;                
+            bool isReinforcementReady = true;             
+
+            
+            int liberationsPerHour = 650;                  
+            double superDestroyerValue = 428500.85;        
+
+            Console.WriteLine("===========================================================================");
+            Console.WriteLine("  _   _ _____ _     _     ____  _____ _    _ _____ ____   ____ ");
+            Console.WriteLine(" | | | | ____| |   | |   |  _ \\| ____| |  | | ____|  _ \\ / ___|");
+            Console.WriteLine(" | |_| |  _| | |   | |   | | | |  _| | |  | |  _| | |_) | |");
+            Console.WriteLine(" |  _  | |___| |___| |___| |_| | |___| |__| | |___|  _ <| |___");
+            Console.WriteLine(" |_| |_|_____|_____|_____|____/|_____|\\____/|_____|_| \\_\\\\____|");
+            Console.WriteLine($"  [{GameTitle} - GALACTIC WAR TERMINAL]");
+            Console.WriteLine("===========================================================================");
+
+            Console.WriteLine("==============================================================");
+            Console.WriteLine("                    HELLDIVER DEPLOYING                      ");
+            Console.WriteLine("==============================================================");
+            Console.WriteLine();
+            Console.WriteLine("                         /\\");
+            Console.WriteLine("                        /  \\");
+            Console.WriteLine("                       /____\\");
+            Console.WriteLine("                      | 00 00 |");
+            Console.WriteLine("                      |  ||   |");
+            Console.WriteLine("                  ____|__||___|____");
+            Console.WriteLine("                 /      ||       \\");
+            Console.WriteLine("                /   ____||____    \\");
+            Console.WriteLine("               /   /          \\    \\");
+            Console.WriteLine("              |   |   HELLDIVER |    |");
+            Console.WriteLine("              |   |             |    |");
+            Console.WriteLine("              |   |_____________|    |");
+            Console.WriteLine("              |        /  \\          |");
+            Console.WriteLine("              |       /____\\         |");
+            Console.WriteLine("              |      /|    |\\        |");
+            Console.WriteLine("              |     / |    | \\       |");
+            Console.WriteLine("              |____/  |    |  \\______|");
+            Console.WriteLine("                    __|    |__");
+            Console.WriteLine("                   /          \\");
+            Console.WriteLine("                  /____________\\");
+            Console.WriteLine();
+            Console.WriteLine("              [ SUPER EARTH HELLDIVER ]");
+            Console.WriteLine("==============================================================");
+
+
+            Console.WriteLine(" |  +-----------------------------------------------------------------+");
+            Console.WriteLine(" |                                                                    |");
+            Console.WriteLine(" |  [MISSION STATUS] -> LIBERATE SUPER EARTH                          |");
+            Console.WriteLine(" |                                                                    |");
+            Console.WriteLine(" |  +-----------------------------------------------------------------+ ");
+            Console.WriteLine("+==============================================================================+");
+            Console.WriteLine("|                    S U P E R   E A R T H                                     |");
+            Console.WriteLine("|                  MINISTRY OF DEFENSE                                         | ");
+            Console.WriteLine("+==============================================================================+");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                  G A L A C T I C   W A R                                     |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine($"|                    {GameTitle,-30}                            |"); 
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("+==============================================================================+");
+
+            Console.WriteLine();
+            Console.WriteLine("+==============================================================================+");
+            Console.WriteLine("|                         MISSION CONTROL                                      |");
+            Console.WriteLine("+==============================================================================+");
+            Console.WriteLine($"|  MISSION STATUS     : [ ACTIVE ]                                             |");
+            Console.WriteLine($"|  OPERATION         : {activeMission,-48}        |");
+            Console.WriteLine("|  FACTION            : SUPER EARTH                                            |");
+            Console.WriteLine("| THREAT LEVEL       : ||||||||||| HIGH                                        |");
+            Console.WriteLine("+==============================================================================+");
+
             Console.WriteLine();
 
-            int hpPercentage = currentHp * 60 / maxHp;
-            Console.WriteLine($"Hp Percentage: {hpPercentage}%");
+            Console.WriteLine("+==============================================================================+");
+            Console.WriteLine("|                        HELLDIVER PROFILE                                     |");
+            Console.WriteLine("+==============================================================================+");
+            Console.WriteLine($"|  CALLSIGN           : {helldiverName,-50}     |");
+            Console.WriteLine($"|  CLASS              : [{soldierClass}] GALACTIC HELLDIVER                                 |");
+            Console.WriteLine($"|  ARMOR GRADE        : GRADE-{armorGrade} COMBAT ARMOR                                   |");
+            Console.WriteLine($"|  COMBAT SPEED       : {combatSpeed,-6} m/s                                             |");
+            Console.WriteLine("|  STATUS             : DEMOCRACY OFFICER                                      |");
+            Console.WriteLine("================================================================================");
+
             Console.WriteLine();
 
-            Console.WriteLine($"Kirin takes 60 damage!");
-            currentHp = currentHp - 60;
+            Console.WriteLine("┌+==============================================================================+");
+            Console.WriteLine("|                         STRATAGEM SYSTEM                                      |");
+            Console.WriteLine("+===============================================================================+");
+            Console.WriteLine($"|  STRATAGEM RANGE    : {stratagemRange,-8} METERS                                         |");
+            Console.WriteLine($"|  REINFORCEMENT      : {(isReinforcementReady ? "[ READY ]" : "[ UNAVAILABLE ]"),-15}                                         |");
+            Console.WriteLine("|  ORBITAL SUPPORT    : [ AVAILABLE ]                                           |");
+            Console.WriteLine("|  EAGLE SUPPORT      : [ AVAILABLE ]                                           |");
+            Console.WriteLine("================================================================================");
+
             Console.WriteLine();
 
-            double heroLevelAsDouble = heroLevel; 
-            Console.WriteLine($"Level as double (implicit): {heroLevelAsDouble}");
+            Console.WriteLine("+===============================================================================+");
+            Console.WriteLine("|                       SUPER EARTH RESOURCES                                   |");
+            Console.WriteLine("+===============================================================================+");
+            Console.WriteLine($"|  REQUISITION SLIPS  : {requisitionSlips:N0} CREDITS                                      |");
+            Console.WriteLine($"|  LIBERATION RATE    : {liberationsPerHour:N0} POINTS / HOUR                                       | ");
+            Console.WriteLine($"|  SUPER DESTROYER    : ${superDestroyerValue:N2} CREDITS                                     |");
+            Console.WriteLine("+===============================================================================+");
 
-            int staminachart = (int)staminaElectric;               
-            int staminaRounded = Convert.ToInt32(staminaElectric);      
-            Console.WriteLine($"Stamina cast (truncates)  : {staminachart}");
-            Console.WriteLine($"Stamina Convert (rounds)  : {staminaRounded}");
+            Console.WriteLine();
+
+           
+
+           
+
+            double liberationRateAsDouble = liberationsPerHour;
+
+            Console.WriteLine("============================================================================");
+            Console.WriteLine("[ SYSTEM LOG ]");
+            Console.WriteLine($"Implicit Conversion : int -> double");
+            Console.WriteLine($"Liberation Rate     : {liberationRateAsDouble}");
+            Console.WriteLine("============================================================================");
+
+           
+            int castShipValue = (int)superDestroyerValue;
+
+          
+            int convertShipValue = Convert.ToInt32(superDestroyerValue);
+
+            Console.WriteLine();
+            Console.WriteLine("[ CONVERSION ANALYSIS ]");
+            Console.WriteLine($"Raw Double Value    : {superDestroyerValue}");
+            Console.WriteLine($"Explicit Cast (int) : {castShipValue}");
+            Console.WriteLine($"Convert.ToInt32()   : {convertShipValue}");
+
+            Console.WriteLine();
+            Console.WriteLine("+============================================================================+");
+            Console.WriteLine("|                                                                            |");
+            Console.WriteLine("|                    D E M O C R A C Y   S E C U R E                         |");
+            Console.WriteLine("|                                                                            |");
+            Console.WriteLine("|                  FOR DEMOCRACY! FOR SUPER EARTH!                           |");
+            Console.WriteLine("|                                                                            |");
+            Console.WriteLine("+============================================================================+");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to return to Super Earth...");
+            Console.ReadKey();
         }
     }
 }
-            
